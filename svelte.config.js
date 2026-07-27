bbc374921aa49338a11911d9017469c99d9fe3f6
thisisissss <somyown@proton.me>
2026-07-27 16:03:53 +0700

    Prerender all routes; fallback to 404.html

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,7 @@ export default {
     adapter: adapter({
       pages: 'build',
       assets: 'build',
-      fallback: 'index.html',
+      fallback: '404.html',
       precompress: false,
       strict: true
     })
